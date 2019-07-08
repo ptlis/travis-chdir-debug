7728fdef29ca2429bbaac2b6a27bba9f1099a886
ptlis <ptlis@ptlis.net>
2019-07-08 22:02:08 +0100

    Add tests for single & two subdirs to demo issue

--- bin-dir/subdir/test.sh
+++ bin-dir/subdir/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo 'success'
+echo 'success subdir 2'
--- tests/SetCwdTest.php
+++ tests/SetCwdTest.php
@@ -5,13 +5,14 @@ final class SetCwdTest extends \PHPUnit\Framework\TestCase
     private const STDOUT = 1;
     private const STDERR = 2;
 
-    private const BINARY_DIR = './bin-dir/subdir';
+    private const BINARY_DIR = './bin-dir';
+    private const BINARY_SUBDIR = './bin-dir/subdir';
     private const COMMAND = './test.sh';
 
     /**
-     * Verify that chdir works correctly with shell_exec.
+     * Verify that chdir works correctly with shell_exec (single subdir).
      */
-    public function testChdirShellExec()
+    public function testChdirShellExecSingleSubdir()
     {
         $prevCwd = getcwd();
         chdir(self::BINARY_DIR);
@@ -19,7 +20,7 @@ final class SetCwdTest extends \PHPUnit\Framework\TestCase
         $stdOut = shell_exec(self::COMMAND);
 
         $this->assertEquals(
-            'success',
+            'success subdir 1',
             trim($stdOut, "\r\n")
         );
 
@@ -27,9 +28,27 @@ final class SetCwdTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * Verify that chdir works correctly with proc_open.
+     * Verify that chdir works correctly with shell_exec (two subdirs).
      */
-    public function testChdirCwdProcOpen()
+    public function testChdirShellExecTwoSubdir()
+    {
+        $prevCwd = getcwd();
+        chdir(self::BINARY_SUBDIR);
+
+        $stdOut = shell_exec(self::COMMAND);
+
+        $this->assertEquals(
+            'success subdir 2',
+            trim($stdOut, "\r\n")
+        );
+
+        chdir($prevCwd);
+    }
+
+    /**
+     * Verify that chdir works correctly with proc_open (single subdir).
+     */
+    public function testChdirCwdProcOpenSingleSubdir()
     {
         $prevCwd = getcwd();
         chdir(self::BINARY_DIR);
@@ -60,7 +79,53 @@ final class SetCwdTest extends \PHPUnit\Framework\TestCase
         $stdErr .= stream_get_contents($pipeList[self::STDERR]);
 
         $this->assertEquals(
-            'success',
+            'success subdir 1',
+            trim($stdOut, "\r\n")
+        );
+
+        $this->assertEquals(
+            '',
+            $stdErr
+        );
+
+        chdir($prevCwd);
+    }
+
+    /**
+     * Verify that chdir works correctly with proc_open (two subdirs).
+     */
+    public function testChdirCwdProcOpenTwoSubdirs()
+    {
+        $prevCwd = getcwd();
+        chdir(self::BINARY_SUBDIR);
+
+        // Setup process with proc_open
+        $pipeList = [];
+        $process = proc_open(
+            self::COMMAND,
+            [
+                self::STDOUT => ['pipe', 'w'],
+                self::STDERR => ['pipe', 'w']
+            ],
+            $pipeList
+        );
+
+        // Poll for completion
+        $status = proc_get_status($process);
+        $stdOut = '';
+        $stdErr = '';
+        while ($status['running']) {
+            $stdOut .= stream_get_contents($pipeList[self::STDOUT]);
+            $stdErr .= stream_get_contents($pipeList[self::STDERR]);
+            $status = proc_get_status($process);
+        }
+
+        // Ensure that any remaining output is read
+        $stdOut .= stream_get_contents($pipeList[self::STDOUT]);
+        $stdErr .= stream_get_contents($pipeList[self::STDERR]);
+
+        $this->assertEquals(
+            'success subdir 2',
             trim($stdOut, "\r\n")
         );
 
