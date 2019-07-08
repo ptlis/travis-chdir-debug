768d3b624ed75be7a438d08d0f829f9492840896
ptlis <ptlis@ptlis.net>
2019-07-08 22:06:46 +0100

    Remove subdir tests

--- tests/SetCwdTest.php
+++ tests/SetCwdTest.php
@@ -28,27 +28,9 @@ final class SetCwdTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * Verify that chdir works correctly with shell_exec (two subdirs).
-     */
-    public function testChdirShellExecTwoSubdir()
-    {
-        $prevCwd = getcwd();
-        chdir(self::BINARY_SUBDIR);
-
-        $stdOut = shell_exec(self::COMMAND);
-
-        $this->assertEquals(
-            'success subdir 2',
-            trim($stdOut, "\r\n")
-        );
-
-        chdir($prevCwd);
-    }
-
-    /**
      * Verify that chdir works correctly with proc_open (single subdir).
      */
-    public function testChdirCwdProcOpenSingleSubdir()
+    public function testChdirCwdProcOpen()
     {
         $prevCwd = getcwd();
         chdir(self::BINARY_DIR);
@@ -90,50 +72,4 @@ final class SetCwdTest extends \PHPUnit\Framework\TestCase
 
         chdir($prevCwd);
     }
-
-//    /**
-//     * Verify that chdir works correctly with proc_open (two subdirs).
-//     */
-//    public function testChdirCwdProcOpenTwoSubdirs()
-//    {
-//        $prevCwd = getcwd();
-//        chdir(self::BINARY_SUBDIR);
-//
-//        // Setup process with proc_open
-//        $pipeList = [];
-//        $process = proc_open(
-//            self::COMMAND,
-//            [
-//                self::STDOUT => ['pipe', 'w'],
-//                self::STDERR => ['pipe', 'w']
-//            ],
-//            $pipeList
-//        );
-//
-//        // Poll for completion
-//        $status = proc_get_status($process);
-//        $stdOut = '';
-//        $stdErr = '';
-//        while ($status['running']) {
-//            $stdOut .= stream_get_contents($pipeList[self::STDOUT]);
-//            $stdErr .= stream_get_contents($pipeList[self::STDERR]);
-//            $status = proc_get_status($process);
-//        }
-//
-//        // Ensure that any remaining output is read
-//        $stdOut .= stream_get_contents($pipeList[self::STDOUT]);
-//        $stdErr .= stream_get_contents($pipeList[self::STDERR]);
-//
-//        $this->assertEquals(
-//            'success subdir 2',
-//            trim($stdOut, "\r\n")
-//        );
-//
-//        $this->assertEquals(
-//            '',
-//            $stdErr
-//        );
-//
-//        chdir($prevCwd);
-//    }
 }
